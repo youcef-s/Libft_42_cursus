2668cda3cbda7318d2c2d4e1bfa5464562a5a688
Youssef Labtaimi <ylabtaim@e1r4p8.1337.ma>
2021-11-03 18:57:44 +0100

    more files

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylabtaim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:33:26 by ylabtaim          #+#    #+#             */
+/*   Updated: 2021/11/03 17:20:55 by ylabtaim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <stdlib.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+#endif
